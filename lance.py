45e93b4fdad341bfc765db5341f48a41416f7d5f
Roman Schuster <schuster@sas.upenn.edu>
2018-07-28 18:47:32 -0400

    Update lance.py

--- lance.py
+++ lance.py
@@ -10,18 +10,25 @@ import requests
 
 API_URL = "https://www.alphavantage.co/query"
 
-data = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": "OLED",
-    "outputsize": "compact",
-    "datatype": "json",
-    "apikey": "7ZXZNBI2F6TB79HK",
-    }
+list_of_requests = []
+tickers = ['CELG', 'MOS', 'VFC', 'ROST', 'ALK']
+
+for ticker in tickers:
+
+    data = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": ticker,
+        "outputsize": "compact",
+        "datatype": "json",
+        "apikey": "7ZXZNBI2F6TB79HK"
+        }
     
-response = requests.get(API_URL, data)
+    list_of_requests.append(data)
+    
+for request in list_of_requests:
+    response = requests.get(API_URL, data)
+    jsonrespo = response.json()
+    jsonrespo_no_meta = jsonrespo['Time Series (Daily)']
+    todaysies = jsonrespo_no_meta['2018-07-20']
+    print(todaysies)
 
-jsonrespo = response.json()
-
-jsonrespo_no_meta = jsonrespo['Time Series (Daily)']
-todaysies = jsonrespo_no_meta['2018-07-20']
-print(todaysies)
