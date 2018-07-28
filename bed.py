20a57ee7dda0b2695676b9269fdf867f6e4cdb64
Roman Schuster <schuster@sas.upenn.edu>
2018-07-28 18:49:42 -0400

    Update bed.py

--- bed.py
+++ bed.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jul 21 23:30:45 2018
-
 @author: roman
 """
 
@@ -10,18 +9,24 @@ import requests
 
 API_URL = "https://www.alphavantage.co/query"
 
-data = {
-    "function": "TIME_SERIES_DAILY",
-    "symbol": "OLED",
-    "outputsize": "compact",
-    "datatype": "json",
-    "apikey": "X856M5SJQGURS91D",
-    }
+list_of_requests = []
+tickers = ['INTU', 'AMGN', 'DISCK', 'C', 'MNST']
+
+for ticker in tickers:
+
+    data = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": ticker,
+        "outputsize": "compact",
+        "datatype": "json",
+        "apikey": "X856M5SJQGURS91D"
+        }
     
-response = requests.get(API_URL, data)
-
-jsonrespo = response.json()
-
-jsonrespo_no_meta = jsonrespo['Time Series (Daily)']
-todaysies = jsonrespo_no_meta['2018-07-20']
-print(todaysies)
+    list_of_requests.append(data)
+    
+for request in list_of_requests:
+    response = requests.get(API_URL, request)
+    jsonrespo = response.json()
+    jsonrespo_no_meta = jsonrespo['Time Series (Daily)']
+    todaysies = jsonrespo_no_meta['2018-07-20']
+    print(todaysies)
